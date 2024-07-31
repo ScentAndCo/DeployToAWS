c31b318cb7424150d5979b273c4c10c642335af3
ScentAndCo <137900132+ScentAndCo@users.noreply.github.com>
2024-07-31 15:41:47 +0100

    Update settings.py

--- shopify_django_app/settings.py
+++ shopify_django_app/settings.py
@@ -16,6 +16,8 @@ from shopify_app import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
